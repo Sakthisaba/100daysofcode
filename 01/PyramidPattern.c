6da87bbe213b226b4d01dbc93c99c3838dc9a4b7
sakthisaba <75113031+Sakthisaba@users.noreply.github.com>
2022-02-25 22:33:38 +0530

    Pyramid

---
+++ 01/PyramidPattern.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void main()
+{
+    
+    int r = 6;
+    int c = 3;
+     for(int i=0;i<r;i++){
+         for(int j = 0;j<=i;j++){
+             printf("*");
+             printf(" ");
+             
+         }
+         printf("\n");
+     }
+    
+   
+
+    
+}
+
+
+//OUTPUT
+//*
+//* *
+//* * * 
+//* * * * 
+//* * * * * 
+//* * * * * * 
