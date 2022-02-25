62579cfbdf73d4c25aaa486e4981ebd81798c160
sakthisaba <75113031+Sakthisaba@users.noreply.github.com>
2022-02-25 22:25:15 +0530

    Create pattern.c

---
+++ 01/pattern.c
@@ -0,0 +1,29 @@
+//CREATE RECTANGLE PATTERN IN C
+#include <stdio.h>
+
+void main()
+{
+    
+    int r = 6;
+    int c = 3;
+     for(int i=0;i<r;i++){
+         for(int j = 0;j<c;j++){
+             printf("*");
+             printf(" ");
+             
+         }
+         printf("\n");
+     }
+    
+   
+
+    
+}
+
+//OUTPUT
+
+// * * *
+// * * *
+// * * *
+// * * *
+// * * *
