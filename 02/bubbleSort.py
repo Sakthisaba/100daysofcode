d23d66adf781c07894f18210cc5d07ae882f08ef
sakthisaba <75113031+Sakthisaba@users.noreply.github.com>
2022-03-01 18:45:02 +0530

    Update bubbleSort.py

--- 02/bubbleSort.py
+++ 02/bubbleSort.py
@@ -2,13 +2,11 @@ arr=[]
 for x in range(4):
   a = int(input("enter a Value"))
   arr.append(a)
-  print(arr)
 
-search = int(input("enter a Value to remove"))
-k  =  0
-for x in range(len(arr)-1) :
-  
-  if(arr[k]== search):
-       print("Element found in")
-      
-  k= k+1
+for i in range(len(arr)-1):
+  for j in range(len(arr)-1):
+   if(arr[j]>arr[j+1]):
+     temp = arr[j+1]
+     arr[j+1] = arr[j]
+     arr[j] =temp
+print(arr) 
